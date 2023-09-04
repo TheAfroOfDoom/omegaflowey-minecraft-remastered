a9df1e78b75b9c18ce8d79de071e55d25ceea0cd
TheAfroOfDoom <tehafroofdoom@gmail.com>
2023-09-03 23:06:13 -0400

    refactor `package-scripts.js` to be more readable

--- package-scripts.js
+++ package-scripts.js
@@ -1,8 +1,15 @@
-const npsUtils = require("nps-utils");
+const { concurrent } = require("nps-utils");
+
+const minecraftPath = "C:/Users/afro/AppData/Roaming/.minecraft";
+const worldName = "omega-flowey-remastered";
+const resourcePackName = "omega-flowey-remastered-resourcepack";
+
+const minecraftWorldPath = `${minecraftPath}/saves/${worldName}`;
+const minecraftResourcePackPath = `${minecraftPath}/resourcepacks/${resourcePackName}`;
 
 module.exports = {
   scripts: {
-    default: npsUtils.concurrent.nps("watch.datapacks", "watch.resourcepack"),
+    default: concurrent.nps("watch.datapacks", "watch.resourcepack"),
     watch: {
       datapacks: 'watch --wait=1 "nps sync.datapacks" datapacks',
       resourcepack: 'watch --wait=1 "nps sync.resourcepack" resourcepack',
@@ -14,16 +21,12 @@ module.exports = {
         "echo syncing resourcepack && nps delete.resourcepack copy.resourcepack && echo finished resourcepack",
     },
     copy: {
-      datapacks:
-        "cpy 'datapacks/**/*' 'C:/Users/afro/AppData/Roaming/.minecraft/saves/omega-flowey-remastered/datapacks'",
-      resourcepack:
-        "cpy 'resourcepack/**/*' 'C:/Users/afro/AppData/Roaming/.minecraft/resourcepacks/omega-flowey-remastered-resourcepack'",
+      datapacks: `cpy 'datapacks/**/*' '${minecraftWorldPath}/datapacks'`,
+      resourcepack: `cpy 'resourcepack/**/*' '${minecraftResourcePackPath}'`,
     },
     delete: {
-      datapacks:
-        "rimraf --glob 'C:/Users/afro/AppData/Roaming/.minecraft/saves/omega-flowey-remastered/datapacks/*'",
-      resourcepacks:
-        "rimraf --glob 'C:/Users/afro/AppData/Roaming/.minecraft/resourcepacks/omega-flowey-remastered-resourcepack/*'",
+      datapacks: `rimraf --glob '${minecraftWorldPath}/datapacks/*'`,
+      resourcepacks: `rimraf --glob '${minecraftResourcePackPath}/*'`,
     },
   },
 };
