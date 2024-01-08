5c129ac448d95960ff6c4c809a09f0731eb86c54
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-01-08 04:20:43 -0500

    add jsdoc for `correct-texture-dimensions` rule

--- package-scripts/linting-rules/correct-texture-dimensions.js
+++ package-scripts/linting-rules/correct-texture-dimensions.js
@@ -3,7 +3,12 @@ const sizeOf = require('image-size');
 const name = 'correct-image-dimensions';
 const applicableExtensions = ['.png'];
 
-const correctImageDimensions = (file) => {
+/**
+ * If a texture's dimensions aren't minimum powers of 16, Minecraft outputs a warning
+ * on resourcepack reload (F3 + T) relating to its mip level being limited.
+ * e.g.: `Texture animated_java:item/black with size 1x1 limits mip level from 4 to 0`
+ */
+const correctTextureDimensions = (file) => {
   // Return early if file does not match any applicable extension
   if (applicableExtensions.every((extension) => !file.endsWith(extension))) {
     return [];
@@ -24,6 +29,6 @@ const correctImageDimensions = (file) => {
 };
 
 module.exports = {
-  function: correctImageDimensions,
+  function: correctTextureDimensions,
   name,
 };
