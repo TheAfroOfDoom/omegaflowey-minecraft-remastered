9794a543304a04a52e68eaedcd5d70555ad8beea
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-01-15 14:12:10 -0500

    close window even on errors now that we're gettings logs - closes #69

--- package-scripts/modules/bb-cli.js
+++ package-scripts/modules/bb-cli.js
@@ -23,13 +23,12 @@ BBPlugin.register('bb-cli', {
         .then(async (module) => {
           await new Promise((resolve) => setTimeout(resolve, 1000));
           await module.script();
-          // TODO(69): move this `window.close()` into a `finally` block once we
-          // fix the inconvenient `console.log` issue
-          window.close();
         })
         .catch((err) => {
-          // consider a finally block that closes window in future and logs if error
           console.log(err);
+        })
+        .finally(() => {
+          window.close();
         });
     }
   },
