314e3cce3dde73b63f19ba43054a24b31af4778a
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-01-15 19:06:46 -0500

    add TODO to fix `yarn start` script watching for changes in AJ export files - https://github.com/TheAfroOfDoom/omega-flowey-minecraft-remastered/issues/70

--- package-scripts/watch-filter.js
+++ package-scripts/watch-filter.js
@@ -1,6 +1,7 @@
 const { minimatch } = require('minimatch');
 const { datapacksGlob, resourcepackGlob } = require('./shared-consts');
 
+// TODO(70): this probably shouldn't be watching Animated Java exported files
 const allowlistFilter = (file) => {
   file = file.replace(/\\/g, '/');
   const glob = file.includes('datapacks') ? datapacksGlob : resourcepackGlob;
