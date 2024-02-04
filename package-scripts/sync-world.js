aeb07d1f99b07b657858bb65ae10e844518bc30b
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-02-04 15:36:57 -0500

    add `sync.down` script to extract `world.zip` to client saves directory - closes #53 - creates a backup in a `tmp` directory so we don't unintentionally lose local world changes - tested by running the following steps:   1. sync-up world with `yarn sync`   2. move player to a different spot in the world (e.g. turn around 180 degrees)   3. sync-down world with `yarn down`   4. open world and see that player is in the original spot before we turned them around

--- package-scripts/sync-world.js
+++ package-scripts/sync-world.js
@@ -1,6 +1,8 @@
 const archiver = require('archiver');
+const chalk = require('chalk');
 const dotenv = require('dotenv');
-const { createWriteStream, remove } = require('fs-extra');
+const extract = require('extract-zip');
+const { createWriteStream, remove, stat, mkdir, exists } = require('fs-extra');
 const minimist = require('minimist');
 
 const { assertEnvironmentVariables } = require('./utils');
@@ -13,11 +15,12 @@ const minecraftPath = process.env.MINECRAFT_PATH;
 const worldName = process.env.WORLD_NAME;
 
 const minecraftWorldPath = `${minecraftPath}/saves/${worldName}`;
+const worldBackupPath = './world.zip';
+const tempBackupPath = './tmp/world-backups';
 
-const syncUp = async () => {
-  await remove('./world.zip');
-  return new Promise((resolve, reject) => {
-    const output = createWriteStream('./world.zip');
+const backupWorld = async (dest, useColor = true) =>
+  new Promise((resolve, reject) => {
+    const output = createWriteStream(dest);
     const archive = archiver('zip');
 
     archive.on('error', function (err) {
@@ -32,7 +35,10 @@ const syncUp = async () => {
     });
 
     output.on('close', function () {
-      console.log('Finished world backup');
+      const prefix = 'Finished world backup to:';
+      const prefixFormatted = useColor ? chalk.greenBright(prefix) : prefix;
+      const destFormatted = chalk.blueBright(dest);
+      console.log(prefixFormatted, destFormatted);
       resolve();
     });
 
@@ -40,10 +46,39 @@ const syncUp = async () => {
     archive.directory(minecraftWorldPath, false);
     archive.finalize();
   });
+
+/** Backup name will be the minecraft save directory's last modified date */
+const createTempWorldBackupName = async () => {
+  const modifiedTime = (await stat(minecraftWorldPath)).mtime;
+  const formattedTime = modifiedTime.toISOString().replaceAll(':', '.');
+  return `world-${formattedTime}.zip`;
+};
+
+const syncUp = async () => {
+  await remove(worldBackupPath);
+  await backupWorld(worldBackupPath);
 };
 
 const syncDown = async () => {
-  throw new Error('Unimplemented');
+  // Create tmp/world-backups folder if it doesn't exist
+  if (!(await exists(tempBackupPath))) {
+    await mkdir(tempBackupPath, { recursive: true });
+  }
+
+  console.log('Backing up your current world as a precautionary measure...');
+  const name = await createTempWorldBackupName();
+  const path = `${tempBackupPath}/${name}`;
+  await backupWorld(path, false);
+
+  await remove(minecraftWorldPath);
+  await extract(worldBackupPath, { dir: minecraftWorldPath });
+
+  const srcFormatted = chalk.blueBright(worldBackupPath);
+  const destFormatted = chalk.blueBright(minecraftWorldPath);
+  const successMessage = chalk.greenBright(
+    `Extracted ${srcFormatted} to ${destFormatted}`,
+  );
+  console.log(successMessage);
 };
 
 const main = async () => {
