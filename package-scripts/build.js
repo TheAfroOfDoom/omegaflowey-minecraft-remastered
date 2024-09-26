30fb825d046fb748a8b20a038381a306f011d7f5
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-09-25 22:15:12 -0400

    🔨 add `build.clean` script to delete build dir

---
+++ package-scripts/build.js
@@ -0,0 +1,115 @@
+const chalk = require('chalk');
+const { copy, emptyDir, pathExists } = require('fs-extra');
+const parseArgs = require('minimist');
+
+const buildDir = './build';
+
+const getSummitDatapackPaths = () => {
+  const attackPaths = prefixPaths('hostile/omega-flowey/attack/', [
+    'bomb',
+    'dentata-snakes',
+    'finger-guns',
+    'friendliness-pellets',
+    'homing-vines',
+    'random',
+    'x-bullets-lower',
+    'x-bullets-shared',
+    'x-bullets-upper',
+    'reset_scores.mcfunction',
+    'tick.mcfunction',
+  ]);
+
+  const entityPaths = prefixPaths('entity/function/', [...attackPaths]);
+
+  const utilsPaths = prefixPaths('utils/function/', [
+    'math/max.mcfunction',
+    'math/min.mcfunction',
+    'error.mcfunction',
+    'log.mcfunction',
+  ]);
+
+  const primaryDatapackPaths = prefixPaths('omega-flowey/', [
+    'pack.mcmeta',
+    ...prefixPaths('data/', [
+      'minecraft',
+      ...entityPaths,
+      'omega-flowey/function/',
+      ...utilsPaths,
+    ]),
+  ]);
+
+  const datapackPaths = prefixPaths('datapacks/', [
+    'animated_java/data',
+    'animated_java/pack.mcmeta',
+    'mathf',
+    'mud',
+    ...primaryDatapackPaths,
+  ]);
+
+  return datapackPaths;
+};
+
+const logInfo = (...data) => {
+  console.log(chalk.yellow('[INFO]'), ...data);
+};
+const logVerbose = (...data) => {
+  console.log(chalk.magenta('[VERBOSE]'), ...data);
+};
+
+const prefixPaths = (prefix, paths) => paths.map((path) => `${prefix}${path}`);
+
+const getDatapackCompilePaths = () => {
+  const { variant } = args;
+  switch (variant) {
+    case 'summit':
+      return getSummitDatapackPaths();
+    default:
+      throw new Error(`Invalid variant: ${variant}`);
+  }
+};
+
+const compileDatapack = async () => {
+  const compiledPath = `${buildDir}/omegaFloweyDatapack`;
+
+  await emptyDir(compiledPath);
+
+  const paths = getDatapackCompilePaths();
+  if (args.verbose) {
+    logVerbose(chalk.bold(chalk.blue('Datapack compile paths:')));
+    for (const src of paths) {
+      logVerbose(chalk.blue('[D]:'), src);
+    }
+  }
+
+  const copySrcToDest = async (src) => {
+    const srcPathExists = await pathExists(src);
+    if (!srcPathExists) {
+      throw new Error(`Source path does not exist: ${chalk.yellow(src)}`);
+    }
+
+    const dest = `${compiledPath}/${src}`;
+    await copy(src, dest);
+  };
+
+  await Promise.all(paths.map(copySrcToDest));
+  logInfo(`Finished copying ${paths.length} ${chalk.blue('datapack')} paths`);
+};
+
+const compile = async () => {
+  await Promise.all([compileDatapack()]);
+};
+
+let args;
+const main = async () => {
+  const minimistOptions = {
+    alias: { v: 'verbose' },
+    default: { variant: 'summit' },
+  };
+  args = parseArgs(process.argv.slice(2), minimistOptions);
+
+  await compile();
+
+  process.exit(0);
+};
+
+main();
