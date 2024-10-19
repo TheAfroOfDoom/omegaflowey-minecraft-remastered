4f00e3be8b8aca916a0f44fa9c5e4e1790ba518a
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-10-19 18:22:42 -0400

    🔨 add channel volume diff db to sound stats

--- package-scripts/list-sound-stats.ps1
+++ package-scripts/list-sound-stats.ps1
@@ -19,10 +19,20 @@ foreach ($script:file in $files) {
   $script:channels = ffprobe -i $file -show_entries stream=channels -select_streams a:0 -of compact=p=0:nk=1 -v 0
   $script:channelsFormatted = "channels: ${channels}"
 
+  $script:channelsVolumeDiffMax = "N/A"
+  if($channels -eq 2) {
+    $channelsVolumeDiffMax = `
+      (
+        ffmpeg -i $file -filter_complex "stereotools=phasel=1[tmp];[tmp]pan=1c|c0=0.5*c0+0.5*c1,volumedetect" -f null - 2>&1
+        | grep max_volume
+      ).split('max_volume: ')[1]
+  }
+  $script:channelsVolumeDiffMaxFormatted = "channelsVolumeDiffMax: ${channelsVolumeDiffMax}"
+
   $script:lufs = ffmpeg -i $file -af ebur128=framelog=verbose -f null - 2>&1 | grep -e "Integrated loudness:" -A 8
   $script:lufsFormatted = ((($lufs.split('\n').where{$_ -ne ''}) | foreach-object -membername trim) -join [environment]::newline)
 
-  $script:statsLine = "# ${relativePath}$([environment]::newline)${channelsFormatted}$([environment]::newline)${lufsFormatted}$([environment]::newline)"
+  $script:statsLine = "# ${relativePath}$([environment]::newline)${channelsVolumeDiffMaxFormatted}$([environment]::newline)${channelsFormatted}$([environment]::newline)${lufsFormatted}$([environment]::newline)"
   if ($out -ne '') {
     $statsLine | out-file -filepath $out -append
   } else {
