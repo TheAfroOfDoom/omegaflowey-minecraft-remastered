1609b8f77d2be6bd9871eca8a26b444473d13418
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-10-31 19:26:21 -0400

    💄 add dynamic heights to timestamps

--- telemetry/src/renderer/src/components/Instance.svelte
+++ telemetry/src/renderer/src/components/Instance.svelte
@@ -21,8 +21,10 @@
       .attr('id', `timeline-${idx}`)
       .attr('width', instanceLength * 20)
 
-    const width = svg.node().getBoundingClientRect().width
-    const timeScale = d3.scaleLinear([0, instanceLength], [0, width])
+    const dimensions = svg.node().getBoundingClientRect()
+    let svgHeight = dimensions.height
+    const svgWidth = dimensions.width
+    const timeScale = d3.scaleLinear([0, instanceLength], [0, svgWidth])
 
     const tickSecs = [0].concat(events.map((e) => (e.tick - instanceStart) / 20))
     const axis = d3
@@ -31,7 +33,33 @@
       .tickFormat(d3.format('.2f'))
       .tickPadding(20)
       .offset(20)
-    svg.append('g').classed('timeline-axis', true).call(axis)
+    const axisElement = svg.append('g').classed('timeline-axis', true).call(axis)
+
+    const minHeight = 20
+    let maxHeight = 0
+    let prevWidth: number
+    let height = minHeight
+    let prevHeight = 0
+    axisElement.selectAll('.tick').each(function () {
+      const transform = d3.select(this).attr('transform')
+      const width = parseFloat(transform.replaceAll(/^translate\(/g, '').replaceAll(/,0\)$/g, ''))
+
+      if (prevWidth === width) {
+        d3.select(this).classed('hidden', true)
+      } else {
+        if (prevWidth !== undefined && width - prevWidth <= 40) {
+          height += 20
+          maxHeight = Math.max(maxHeight, height)
+        } else {
+          height = minHeight
+        }
+        d3.select(this).attr('transform', `translate(${width}, ${height})`)
+      }
+
+      prevHeight = height
+      prevWidth = width
+    })
+    svg.attr('height', maxHeight + svgHeight)
 
     svg = svg.node()
   })
