cdee0fabc78355c0183cbbdec7dc0a49cf6e9c0d
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-10-31 13:41:14 -0400

    🎉 add boilerplate electron-vite app - too many frameworks dawg

---
+++ telemetry/src/renderer/src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Versions from './components/Versions.svelte'
+  import electronLogo from './assets/electron.svg'
+
+  const ipcHandle = (): void => window.Electron.ipcRenderer.send('ping')
+</script>
+
+<img alt="logo" class="logo" src={electronLogo} />
+<div class="creator">Powered by electron-vite</div>
+<div class="text">
+  Build an Electron app with
+  <span class="svelte">Svelte</span>
+  and
+  <span class="ts">TypeScript</span>
+</div>
+<p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
+<div class="actions">
+  <div class="action">
+    <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
+  </div>
+  <div class="action">
+    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
+    <a target="_blank" rel="noreferrer" on:click={ipcHandle}>Send IPC</a>
+  </div>
+</div>
+<Versions />
