749ca39c0adbbf97d105a1e15086df757c243b8e
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-10-31 01:53:29 -0400

    🔨 convert esbuild to watch script

--- telemetry/scripts/esbuild.cjs
+++ telemetry/scripts/esbuild.cjs
@@ -1,10 +1,17 @@
-const { build } = require("esbuild");
-build({
-  entryPoints: ["src/main.ts"],
-  outfile: "dist/main.js",
-  bundle: true,
-  platform: "node",
-  packages: "external",
-}).then(() => {
-  console.log("done building");
-});
+const { context } = require("esbuild");
+
+async function main() {
+  const ctx = await context({
+    entryPoints: ["src/main.ts"],
+    outfile: "dist/main.js",
+    bundle: true,
+    platform: "node",
+    packages: "external",
+  });
+
+  await ctx.watch();
+
+  console.log("esbuild watching");
+}
+
+main();
