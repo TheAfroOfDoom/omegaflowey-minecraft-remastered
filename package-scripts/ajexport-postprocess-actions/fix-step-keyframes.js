e924530de30dc81f1d9efb699f5980e1158192ee
Afro <tehafroofdoom@gmail.com>
2025-12-28 14:59:52 -0500

    💫 fix step keyframes for specific mouth-laugh frames

--- package-scripts/ajexport-postprocess-actions/fix-step-keyframes.js
+++ package-scripts/ajexport-postprocess-actions/fix-step-keyframes.js
@@ -2,9 +2,7 @@ const chalk = require('chalk');
 const { lstatSync, readFileSync, writeFileSync } = require('fs');
 const { globSync } = require('glob');
 
-const setBonesInterpolationDurationZero = (rootDir) => {
-  console.log(`Setting step-animation bones' interpolation duration to 0...`);
-
+const setInterpolationDurationZeroAllFrames = (rootDir) => {
   const stepAnimations = [
     ['omegaflowey_arm_vine', 'omegaflowey_intro_shake'],
     ['omegaflowey_arm_vine_right', 'omegaflowey_intro_shake'],
@@ -47,8 +45,73 @@ const setBonesInterpolationDurationZero = (rootDir) => {
   }
 };
 
+const setInterpolationDurationMouthFrames = (rootDir) => {
+  const stepAnimations = [['omegaflowey_mouth', 'omegaflowey_laugh']];
+  const frames = [
+    1, 4,
+
+    8, 11, 14,
+
+    18, 21, 24,
+
+    28, 31, 34,
+
+    38, 41, 44,
+
+    48, 51, 54,
+
+    58, 61, 64,
+
+    68, 71, 74,
+
+    78, 81, 84,
+
+    88, 91, 94,
+
+    98, 101, 104,
+
+    108, 111, 114,
+
+    118,
+  ];
+
+  const stepAnimationDirs = stepAnimations.map((stepAnimationPair) => {
+    const [namespace, animation] = stepAnimationPair;
+    return `${rootDir}/function/${namespace}/animations/${animation}`;
+  });
+
+  for (const dir of stepAnimationDirs) {
+    process.stdout.write(chalk.gray(`${dir} ... `));
+    let numChanges = 0;
+
+    const frameFiles = frames.map(
+      (frame) => `${dir}/zzz/frames/${frame}.mcfunction`,
+    );
+
+    for (const frameFile of frameFiles) {
+      const content = readFileSync(frameFile, 'utf8');
+      const lines = content.split('\n');
+      const fixedLines = lines.map((line) => {
+        if (line.endsWith('interpolation_duration: 1}')) {
+          numChanges += 1;
+          return line.replace(
+            /interpolation_duration: 1}$/,
+            'interpolation_duration: 0}',
+          );
+        } else {
+          return line;
+        }
+      });
+      writeFileSync(frameFile, fixedLines.join('\n'));
+    }
+    console.log(chalk.greenBright(numChanges));
+  }
+};
+
 const fixStepKeyframes = (rootDir) => {
-  setBonesInterpolationDurationZero(rootDir);
+  console.log(`Setting step-animation bones' interpolation duration to 0...`);
+  setInterpolationDurationZeroAllFrames(rootDir);
+  setInterpolationDurationMouthFrames(rootDir);
 };
 
 module.exports = fixStepKeyframes;
