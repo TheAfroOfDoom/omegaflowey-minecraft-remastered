4437a80ae0605a9926e60a8de9831c9e7a91294d
Afro <tehafroofdoom@gmail.com>
2026-05-17 17:12:53 -0400

    💄 fix emissive for tv-screen static + warning screens - it's technically not perfect for the `warning` screen animation when it's a black screen, but it's good enough

--- package-scripts/ajexport-postprocess-actions/disable-shading.js
+++ package-scripts/ajexport-postprocess-actions/disable-shading.js
@@ -18,6 +18,8 @@ const disableShading = ({ resourcePackRootDir }) => {
     ['omegaflowey_soul', 'root', '3'],
     ['omegaflowey_soul', 'root', '4'],
     ['omegaflowey_soul', 'root', '5'],
+    ['omegaflowey_tv_screen', 'screen', 'static'],
+    ['omegaflowey_tv_screen', 'screen', 'warning'],
   ];
 
   const tints = [{ type: 'minecraft:constant', value: 66046 }];
