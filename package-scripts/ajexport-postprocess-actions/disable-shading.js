93d8ad2694957d6c69c68fe26577a7301d413f5b
Afro <tehafroofdoom@gmail.com>
2026-05-18 00:29:31 -0400

    💄 fix emissive for tv-screen thumbs_up

--- package-scripts/ajexport-postprocess-actions/disable-shading.js
+++ package-scripts/ajexport-postprocess-actions/disable-shading.js
@@ -46,6 +46,7 @@ const disableShading = ({ resourcePackRootDir }) => {
     ['omegaflowey_tv_screen', 'screen', 'soul_shatter_9'],
     ['omegaflowey_tv_screen', 'screen', 'soul_shatter_10'],
     ['omegaflowey_tv_screen', 'screen', 'static'],
+    ['omegaflowey_tv_screen', 'screen', 'thumbs_up'],
     ['omegaflowey_tv_screen', 'screen', 'warning'],
     ['omegaflowey_tv_screen', 'screen_tvinside', 'tvinside_0'],
     ['omegaflowey_tv_screen', 'screen_tvinside', 'tvinside_1'],
